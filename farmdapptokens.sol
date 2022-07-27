7d02a7c969550c994d46847193b188587651f584
bfinance <96443938+bfinance@users.noreply.github.com>
2022-07-27 13:09:25 -0500

    Update farmdapptokens.sol

--- farmdapptokens.sol
+++ farmdapptokens.sol
@@ -2,10 +2,7 @@
 
 pragma solidity ^0.8.1;
 
-import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
